4713aa8adf76c1a0452a6e508e3da72ab70f90aa
KaleeswariR <kaleeswari090204@gmail.com>
2024-01-22 11:45:34 +0530

    initial

--- pages/audio_player.py
+++ pages/audio_player.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
+
 def main():
     st.title("Audio Player App")
-    audio_file_path = "ilakana-kavithai.mp3"
+    audio_file_path = "C:\ilakana-kavithai.mp3"
     st.audio(audio_file_path, format='audio/mp3')
+
+
 if __name__ == "__main__":
     main()
