7f51491c952918aa1bd32703fed1266208649d51
KaleeswariR <kaleeswari090204@gmail.com>
2024-01-22 12:55:37 +0530

    initial commit

--- pages/audio_player.py
+++ pages/audio_player.py
@@ -3,7 +3,10 @@ import streamlit as st
 
 def main():
     st.title("Audio Player App")
-    audio_file_path = "C:\ilakana-kavithai.mp3"
+
+    # Update the path to the correct relative path
+    audio_file_path = "pages/ilakana-kavithai.mp3"
+
     st.audio(audio_file_path, format='audio/mp3')
 
 
